1fcb023c8c669b28dc35937b6d0eaf5a990af3f3
Marly Gotti <marlygotti@gmail.com>
2022-05-15 04:28:19 -0400

    Remove package installation.

--- global.R
+++ global.R
@@ -1,94 +1,34 @@
-# Packages needed for the app.
-packages = c("shiny"
-             ,"shinyhelper"
-             ,"shinyjs"
-             ,"shinydashboard"
-             ,"shinyWidgets"
-             ,"data.table"
-             ,"DT"
-             ,"readr"
-             ,"lubridate"
-             ,"RSQLite"
-             ,"DBI"
-             ,"rvest"
-             ,"xml2"
-             ,"httr"
-             ,"desc"
-             ,"dplyr"
-             ,"tools"
-             ,"stringr"
-             ,"tidyverse"
-             ,"loggit"
-             ,"rAmCharts"
-             ,"devtools"
-             ,"plotly"
-             ,"cranlogs"
-             ,"formattable"
-             ,"rintrojs"
-             ,"shinymanager"
-             ,"keyring"
-             ,"rstudioapi"
-             ,"glue"
-             ,"bslib"
-             ,'waiter'
-             ,'riskmetric'
-)
-
-# Load required packages.
-package.check <- lapply(
-  packages, FUN = function(x) { library(x, character.only = TRUE) }
-)
-
-# Install and load required packages.
-# package.check <- lapply(
-#   packages,
-#   FUN = function(x) {
-#     if (!require(x, character.only = TRUE)) {
-#       install.packages(x, dependencies = TRUE)
-#       library(x, character.only = TRUE)
-#     }
-#   }
-# )
-
-# Install a specific version of riskmetric from GitHub and load it.
-# if(!require(riskmetric)){
-#   devtools::install_github("pharmaR/riskmetric", ref = "release-v0.1.1")
-#   library(riskmetric)
-# }
-
-
-# Note: If deploying the app to shinyapps.io, then the code to directly install
-# missing packages will need to be removed as the app will fail to deploy.
-# Instead comment the code that install packages and attach them directly by
-# uncommenting the lines below.
-
-# library(shiny)
-# library(shinyhelper)
-# library(shinyjs)
-# library(shinydashboard)
-# library(shinyWidgets)
-# library(data.table)
-# library(DT)
-# library(readr)
-# library(lubridate)
-# library(RSQLite)
-# library(DBI)
-# library(rvest)
-# library(xml2)
-# library(httr)
-# library(desc)
-# library(dplyr)
-# library(tools)
-# library(stringr)
-# library(tidyverse)
-# library(loggit)
-# library(shinycssloaders)
-# library(rAmCharts)
-# library(devtools)
-# library(plotly)
-# library(riskmetric) # devtools::install_github("pharmaR/riskmetric")
-# library(formattable)
-# library(rintrojs)
+# Load needed packages.
+library(shiny)
+library(shinyhelper)
+library(shinyjs)
+library(shinydashboard)
+library(shinyWidgets)
+library(data.table)
+library(DT)
+library(readr)
+library(lubridate)
+library(RSQLite)
+library(DBI)
+library(rvest)
+library(xml2)
+library(httr)
+library(desc)
+library(dplyr)
+library(tools)
+library(stringr)
+library(tidyverse)
+library(loggit)
+library(shinycssloaders)
+library(rAmCharts)
+library(devtools)
+library(plotly)
+library(riskmetric) # devtools::install_github("pharmaR/riskmetric")
+library(formattable)
+library(rintrojs)
+library(bslib)
+library(shinymanager)
+library(glue)
 
 passphrase <- 'somepassphrase'
 
